8c0df41520632c1e0d5a75acdf946b4d217aa6d5
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-16 18:45:34 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function TRACK()
 {
-  var dimension1 = document.getElementById('id1').value;
-  var dimension2 = document.getElementById('id2').value;
-  var dimension3 = document.getElementById('id3').value;
+  var dimension1 = document.getElementById("value1").value;
+  var dimension2 = document.getElementById("value2").value;
+  var dimension3 = document.getElementById("value3").value;
   var sides = ["dimension1","dimension2","dimension3"];
   var sideA = parseInt("dimension1");
   var sideB = parseInt("dimension2");
   var sideC = parseInt("dimension3");
 
   if (sideA === sideB && sideB === sideC){
-    window.alert("Equilateral Triangle");
+    alert("Equilateral Triangle");
   }
 }
