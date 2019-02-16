8e434ca2c10a53b9ea3bb4180b3b819ad6d1aa42
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-16 22:07:16 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function check(){
   var dimension1 = document.getElementById("d1").value;
   var dimension2 = document.getElementById("d2").value;
   var dimension3 = document.getElementById("d3").value;
-  var sides = ["dimension1","dimension2","dimension3"];
+  var sides = [dimension1,dimension2,dimension3];
   var sideA = parseInt("dimension1");
   var sideB = parseInt("dimension2");
   var sideC = parseInt("dimension3");
