130d3e90a2f3bf640f729bee62faaba75f67c994
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-17 14:11:02 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function check(){
-  var dimension1 = document.getElementById("d1").value;
-  var dimension2 = document.getElementById("d2").value;
-  var dimension3 = document.getElementById("d3").value;
-  var sides = ["dimension1","dimension2","dimension3"];
-  var sideA = parseInt("dimension1");
-  var sideB = parseInt("dimension2");
-  var sideC = parseInt("dimension3");
+  var length1 = document.getElementById("data1").value;
+  var length2 = document.getElementById("data2").value;
+  var length3 = document.getElementById("data3").value;
+  var sides = ["sideA","sideB","sideC"];
+  var sideA = parseInt("length1");
+  var sideB = parseInt("length2");
+  var sideC = parseInt("length3");
   if (sideA === sideB && sideB === sideC){
     alert("Equilateral Triangle");
   }else if (sideA === sideB && sideA !== sideC){
